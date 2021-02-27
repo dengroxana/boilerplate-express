ed21a73112c399dd03fb1bc98cf1687b32afbf86
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2021-02-27 18:35:56 -0500

    adding env variables

--- myApp.js
+++ myApp.js
@@ -1,10 +1,23 @@
 var express = require("express");
 var app = express();
+var dotenv = require("dotenv").config();
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({
+      message: "Hello json".toUpperCase(),
+    });
+  } else {
+    res.json({
+      message: "Hello json",
+    });
+  }
+});
 
 module.exports = app;
